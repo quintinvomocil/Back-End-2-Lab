411fb061938145d926de5201e3229f85dc084c9e
Quintin Vomocil <quintinvomocil@gmail.com>
2022-07-06 15:27:30 -0600

    All done.

--- server/server.js
+++ server/server.js
@@ -8,4 +8,9 @@ app.use(cors());
 
 const {getHouses, deleteHouse, createHouse, updateHouse} = require("./controller.js")
 
+app.get('/api/houses', getHouses);
+app.delete('/api/houses/:id', deleteHouse);
+app.post('/api/houses', createHouse);
+app.put('/api/houses/:id', updateHouse);
+
 app.listen(4004, () => console.log("Server is running properly."))
